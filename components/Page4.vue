4c25154d1dd8e1db9d035422c3356405087f157e
Sara Vieira <hey@iamsaravieira.com>
2018-05-13 15:15:05 +0200

    add gifs

--- components/Page4.vue
+++ components/Page4.vue
@@ -3,11 +3,15 @@
     <div>
       <img class="logo" src="../assets/dope.gif" alt="spaceship" />
     </div>
+    <img class="image1" src="../assets/debuggingthemind.gif" />
+    <img class="image2" src="../assets/frontend1.gif" />
+    <img class="image3" src="../assets/pizzalove.gif" />
   </section>
 </template>
 
 <style scoped>
 .container {
+    position: relative;
     min-height: 100vh;
     width: 100vw;
     display: flex;
@@ -15,5 +19,24 @@
     align-items: center;
     text-align: center;
     background: url('../assets/bg4.png');
+}
+
+img {
+    position: absolute;
+}
+
+.image3 {
+    right: 0;
+    top: 0;
+}
+
+.image2 {
+    left: 0;
+    top: 0;
+}
+
+.logo {
+    position: relative;
+    z-index: 10;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,12 @@
     <div class="container">
       <Page4 />
     </div>
-
+    <h1 class="center subtitle">
+      Inspiration Websites:
+    </h1>
+    <div class="container">
+      <a :key="website" v-for="website in websites" class="link" :href="website">{{website}}</a>
+    </div>
   </div>
 </template>
 
@@ -29,12 +34,69 @@
 import Page2 from '~/components/Page2.vue';
 import Page3 from '~/components/Page3.vue';
 import Page4 from '~/components/Page4.vue';
+import microlink from 'microlinkjs';
 
 export default {
     components: {
         Page2,
         Page3,
         Page4
+    },
+    data() {
+        return {
+            websites: [
+                'http://thatsthefinger.com/',
+                'http://hooooooooo.com/',
+                'http://tinytuba.com/',
+                'http://cant-not-tweet-this.com/',
+                'http://www.staggeringbeauty.com/',
+                'http://eelslap.com/',
+                'http://corndog.io/',
+                'http://burymewithmymoney.com/',
+                'http://www.trypap.com/',
+                'https://css-to-my-html.now.sh/',
+                'http://www.fallingfalling.com/',
+                'http://www.republiquedesmangues.fr/',
+                'https://www.lingscars.com/',
+                'http://www.partridgegetslucky.com/',
+                'http://weirdorconfusing.com/',
+                'http://www.rrrgggbbb.com/',
+                'http://www.koalastothemax.com/',
+                'http://beesbeesbees.com/',
+                'https://dan.church/',
+                'http://cat-bounce.com/',
+                'http://r33b.net/',
+                'http://www.sanger.dk/',
+                'http://chrismckenzie.com/',
+                'http://www.everydayim.com/',
+                'https://isxmasafriday13.now.sh/',
+                'http://endless.horse/',
+                'http://corndogoncorndog.com/',
+                'http://www.pointerpointer.com/',
+                'http://www.ismycomputeron.com/',
+                'http://ninjaflex.com/',
+                'http://metaphorsofinfinity.com/',
+                'http://imaninja.com/',
+                'http://ihasabucket.com/',
+                'https://corgiorgy.com/',
+                'http://iamawesome.com/',
+                'http://www.electricboogiewoogie.com/',
+                'http://unicodesnowmanforyou.com/',
+                'http://www.wutdafuk.com/',
+                'http://www.nullingthevoid.com/',
+                'https://www.cameronsworld.net/',
+                'http://zombo.com/',
+                'http://onemillionlols.com/',
+                'https://www.omfgdogs.com/',
+                'http://pixelsfighting.com/',
+                'http://www.ascii-middle-finger.com/',
+                'http://buzzybuzz.biz/',
+                'http://yeahlemons.com/'
+            ]
+        };
+    },
+    mounted() {
+        microlink('.link');
     }
 };
 </script>
@@ -43,9 +105,15 @@ export default {
 .container {
     min-height: 100vh;
     display: flex;
+    flex-wrap: wrap;
     justify-content: center;
     align-items: center;
     text-align: center;
+}
+
+.link {
+    width: 125px;
+    margin: 10px;
 }
 
 h1 {
@@ -56,6 +124,9 @@ h2 {
     margin-top: 20px;
 }
 
+.center {
+    text-align: center;
+}
 .logo {
     width: 200px;
 }
@@ -65,13 +136,5 @@ section {
     align-items: center;
     justify-content: center;
     flex-direction: column;
-}
-
-.arrowAnim {
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
 }
 </style>
