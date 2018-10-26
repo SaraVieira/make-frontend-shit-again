4d98d8b1848e68dddceccdace4bc4460689f4070
Alexander Pateenok <pateenoc@gmail.com>
2018-10-26 17:09:39 +0300

    Add turbo.fish (#37)

--- pages/data.js
+++ pages/data.js
@@ -71,5 +71,6 @@ export default [
   "http://www.internetfortheday.com/",
   "http://www.akrepnalan.com/",
   "https://bxyoung89.github.io/xorberax-visualizer/",
-  "http://finger-frenzy.now.sh/"
+  "http://finger-frenzy.now.sh/",
+  "https://turbo.fish/"
 ]
